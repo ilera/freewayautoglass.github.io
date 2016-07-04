be6fafc266ffe11f0d1b4407d5a13d6c1c1df78f
ilera <ilera@asu.edu>
2016-07-03 20:35:10 -0700

    switch to port 80

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var port = process.env.PORT || 3000;
 
 app.get('/home', function (req, res) {
 var html = fs.readFileSync('freewayautoglass.html', 'utf-8');
   res.send(html);
 });
 
-app.listen(80, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
